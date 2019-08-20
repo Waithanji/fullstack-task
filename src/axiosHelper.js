b7dfa0a6d67f2705c30e84be46ceda5958ffd161
Dankimbugua <keem2567@gmail.com>
2019-08-20 15:53:48 +0300

    change base url

--- src/axiosHelper.js
+++ src/axiosHelper.js
@@ -6,7 +6,7 @@ var jsonUser = user == null ? "" : JSON.parse(user)
 var token =  jsonUser != "" ? jsonUser.api_token : "";
 
 const instance = axios.create({
-    baseURL: 'http://142.93.26.83/fullstack-task-api/public/',
+    baseURL: 'http://142.93.26.83/fullstack-task-api/public/api/v1',
     headers: {'x-api-key': token, 'Content-Type':'application/json'}
 })
 
